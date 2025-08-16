5d79838fef72c5f2bfa645d7c6a88f25421cf99e
Sho-jii <jarib.sioco2004s@gmail.com>
2025-08-16 12:52:52 +0800

    Added AddController for [purpose of the controller]

---
+++ app/controllers/Add.php
@@ -0,0 +1,14 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+/**
+ * Controller: Add
+ * 
+ * Automatically generated via CLI.
+ */
+class Add extends Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
